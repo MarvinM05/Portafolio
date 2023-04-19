118ab63b247d88e690e275f8149941fccfa587d3
Marvin Méndez Muñoz <m.mendez@teletica.com>
2023-04-19 17:31:05 -0600

    mejoras

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,38 +15,3 @@ window.addEventListener("scroll", function () {
 });
 
 
-// const formulario = document.querySelector("#form");
-// const sendForm = document.querySelector("#mail");
-
-// formulario.addEventListener("submit", handleSubmit);
-
-// function handleSubmit(e) {
-//   e.preventDefault();
-//   const form = new FormData(this);
-//   sendForm.setAttribute(
-//     "href",
-//     `mailto:test@correo.com?subject=${form.get("name")} - ${form.get(
-//       "subject"
-//     )}&body=${form.get("message")}`
-//   );
-//   sendForm.click();
-// }
-// let lastKnownScrollPosition = 0;
-// let ticking = false;
-
-// function doSomething(scrollPos) {
-//   // Do something with the scroll position
-// }
-
-// document.addEventListener("scroll", (event) => {
-//   lastKnownScrollPosition = window.scrollY;
-
-//   if (!ticking) {
-//     window.requestAnimationFrame(() => {
-//       doSomething(lastKnownScrollPosition);
-//       ticking = false;
-//     });
-
-//     ticking = true;
-//   }
-// });
